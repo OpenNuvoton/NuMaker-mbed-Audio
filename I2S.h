3a58d4f9996574a0219118d7983795feef9baf89
MS30 CSYang2 <CSYang2@nuvoton.com>
2017-03-28 19:47:31 +0800

    Add new constructor: set I2S as masster or slave

--- I2S.h
+++ I2S.h
@@ -23,15 +23,24 @@ class I2S
 {
     public:
         I2S(PinName i2s_tx, PinName i2s_rx, PinName i2s_bclk, PinName i2s_mclk, PinName i2s_lrclk);
+        I2S(PinName i2s_tx, PinName i2s_rx, PinName i2s_bclk, PinName i2s_mclk, PinName i2s_lrclk, char i2s_master_enable);
         void start(void);
+        void record(void);
         void stop(void);
         void write(int* buffer, int from,  int length);
         void read(void);
         void attach(void(*fptr)(void));
-        int status(void);
+        int  status(void);
+        void format(int rate, char count, char length);
         
         int rxBuffer[4];
-
+        
+    private:
+        int  m_samplingRate;
+        char m_channelCount;
+        char m_sampleBitLength;
+        char m_masterMode;
+        
 };
 
 #endif /*__MBED_I2S_H__*/
